68720fbb44ec8253b3e903b5bbb48cb32b692079
VENU KAUSHIK <venukaushik31@gmail.com>
2023-09-27 19:31:50 +0530

    login forn
    login form using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <div className="app">
+      <div className="login">
+        <div className='heading'>
+          <h1>Login</h1>
+        </div>
+        <div className="container">
+          <div className="socials">
+            <i className="fab fa-google"></i>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-facebook-square"></i>
+            <i className="fab fa-twitter-square"></i>
+            <i className="fab fa-github-square"></i>
+          </div>
+        </div>
+      </div>
+    </div >
+  </>
+  )
 }
 
-export default App;
+export default App
