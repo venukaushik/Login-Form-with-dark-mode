2cc50be3e737def1d2af755c90b9a4866f2a41ec
VENU KAUSHIK <venukaushik31@gmail.com>
2023-09-28 22:00:34 +0530

    favicon updation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css'
+import Login from './Components/Login';
 
 function App() {
 
@@ -9,14 +10,7 @@ function App() {
 
   const [modeText, setModeText] = useState('Dark')
 
-
-  // const toggleTheme = () => {
-  //   const newMode = mode === 'dark' ? 'light' : 'dark';
-  //   setMode(newMode);
-  //   setModeText('dark' ? 'Light' : 'Dark')
-  // }
-
-  const toggleTheme = () => {
+  let toggleTheme = () => {
     if (mode === 'dark') {
       setMode('light')
       setModeText('Dark ')
@@ -28,47 +22,7 @@ function App() {
     }
   }
   return (
-    <div className="app" data-theme={mode}>
-      <div className="login">
-        <h1>Login</h1>
-        <div className="container">
-          <div className="top">
-            <i className="fab fa-google"></i>
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-facebook-square"></i>
-            <i className="fab fa-twitter-square"></i>
-            <i className="fab fa-github-square"></i>
-          </div>
-          <p className="divider"><span>Or</span></p>
-          <form>
-            <label>E-mail</label>
-            <input type="email" placeholder='Enter your email' />
-            <label>Password</label>
-            <input type="password" placeholder='Enter your password' />
-
-            <div className="remember">
-              <input type="checkbox" name="remember" id="remember" />
-              <label>Remember Me</label>
-            </div>
-
-            <button>Log In</button>
-
-          </form>
-
-          <div className="bottom">
-            <p>Forgot your password?</p>
-            <a href="/">Reset Password</a>
-          </div>
-
-          <p className="create">Create Account</p>
-        </div>
-
-        <div className="theme-toggle">
-          <h2>{modeText} Theme</h2>
-          <i onClick={toggleTheme} className={modeIcon}></i>
-        </div>
-      </div>
-    </div >
+    <Login mode={mode} modeIcon={modeIcon} modeText={modeText} />
   )
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Login(props) {
+    return (
+        <div className="app" data-theme={props.mode}>
+            <div className="login">
+                <h1>Login</h1>
+                <div className="container">
+                    <div className="top">
+                        <i className="fab fa-google"></i>
+                        <i className="fab fa-linkedin"></i>
+                        <i className="fab fa-facebook-square"></i>
+                        <i className="fab fa-twitter-square"></i>
+                        <i className="fab fa-github-square"></i>
+                    </div>
+                    <p className="divider"><span>Or</span></p>
+                    <form>
+                        <label>E-mail</label>
+                        <input type="email" placeholder='Enter your email' />
+                        <label>Password</label>
+                        <input type="password" placeholder='Enter your password' />
+
+                        <div className="remember">
+                            <input type="checkbox" name="remember" id="remember" />
+                            <label>Remember Me</label>
+                        </div>
+
+                        <button>Log In</button>
+
+                    </form>
+
+                    <div className="bottom">
+                        <p>Forgot your password?</p>
+                        <a href="/">Reset Password</a>
+                    </div>
+
+                    <p className="create">Create Account</p>
+                </div>
+
+                <div className="theme-toggle">
+                    <h2>{props.modeText} Theme</h2>
+                    <i onClick={props.toggleTheme} className={props.modeIcon}></i>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Login
